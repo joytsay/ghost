f7f1b20e4da6f99948f6ba8c20c0f6cdf412169b
joy.tsay <joy.tsay@dhtcl.com>
2024-04-25 16:50:50 +0800

    add HEARnet

---
+++ train_hearnet.py
@@ -0,0 +1,219 @@
+print("started imports")
+
+import sys
+import argparse
+import time
+import cv2
+import wandb
+import os
+import torchvision
+import numpy as np
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import torch.nn.functional as F
+import torch
+import torch.optim.lr_scheduler as scheduler
+
+# custom imports
+sys.path.append('./apex/')
+
+from apex import amp
+from network.AEI_Net import *
+from network.HEAR_Net import *
+from utils.training.Dataset import AugmentedOcclusions
+from onnx2torch import convert
+
+print("finished imports")
+
+MSE = torch.nn.MSELoss()
+L1 = torch.nn.L1Loss()
+
+
+def get_numpy_image(X):
+    X = X[:8]
+    X = torchvision.utils.make_grid(X.detach().cpu(), nrow=X.shape[0]).numpy() * 0.5 + 0.5
+    X = X.transpose([1,2,0])*255
+    np.clip(X, 0, 255).astype(np.uint8)
+    return X
+
+
+def make_image(Xs, Xt, Y):
+    Xs = get_numpy_image(Xs)
+    Xt = get_numpy_image(Xt)
+    Y = get_numpy_image(Y)
+    return np.concatenate((Xs, Xt, Y), axis=0).transpose([2, 0, 1])
+
+def train_one_epoch(G: 'generator model', 
+                    net: 'hearnet model', 
+                    opt: "hearnet_opt", 
+                    netArc: 'netArc model',
+                    args: 'Args Namespace',
+                    dataloader: torch.utils.data.DataLoader,
+                    device: 'torch device',
+                    epoch:int):
+    show_step = args.show_step
+    save_epoch = args.save_epoch
+    for iteration, data in enumerate(dataloader):
+        if args.max_steps > 0 and iteration > args.max_steps:
+            break
+        start_time = time.time()
+        
+        Xs, Xt, same_person = data
+        Xs = Xs.to(device)
+        Xt = Xt.to(device)
+        with torch.no_grad():
+            embed_s = netArc(F.interpolate(Xs, [112, 112], mode='bilinear', align_corners=False))
+            embed_t = netArc(F.interpolate(Xt, [112, 112], mode='bilinear', align_corners=False))
+        same_person = same_person.to(device)
+
+        # train HEAR
+        opt.zero_grad()
+        with torch.no_grad():
+            Yst_hat, _ = G(Xt, embed_s)
+            Ytt, _ = G(Xt, embed_t)
+
+        dYt = Xt - Ytt
+        hear_input = torch.cat((Yst_hat, dYt), dim=1)
+        Yst = net(hear_input)
+
+        Yst_aligned = Yst
+
+        id_Yst = netArc(F.interpolate(Yst_aligned, [112, 112], mode='bilinear', align_corners=True))
+
+        L_id = (1 - torch.cosine_similarity(embed_s, id_Yst, dim=1)).mean()
+
+        L_chg = L1(Yst_hat, Yst)
+
+        L_rec = torch.sum(0.5 * torch.mean(torch.pow(Yst - Xt, 2).reshape(args.batch_size, -1), dim=1) * same_person) / (same_person.sum() + 1e-6)
+
+        loss = L_id + L_chg + L_rec
+        with amp.scale_loss(loss, opt) as scaled_loss:
+            scaled_loss.backward()
+
+        # loss.backward()
+        opt.step()
+
+        batch_time = time.time() - start_time
+        if iteration % show_step == 0:
+            image = make_image(Xs, Xt, Yst)
+            if args.use_wandb:
+                wandb.log({"gen_images":wandb.Image(image, caption=f"{epoch:03}" + '_' + f"{iteration:06}")})
+            else:
+                cv2.imwrite('./images/HEAR_generated_image.jpg', image.transpose([1,2,0])[:,:,::-1])
+        print(f'epoch: {epoch}    {iteration} / {len(dataloader)}')
+        print(f'loss: {loss.item()} batch_time: {batch_time}s')
+        print(f'L_id: {L_id.item()} L_chg: {L_chg.item()} L_rec: {L_rec.item()}')
+        if args.use_wandb:
+            wandb.log({"loss": loss.item(),
+                       "L_id": L_id.item(),
+                       "L_chg": L_chg.item(),
+                       "L_rec": L_rec.item()})
+        if iteration % save_epoch == 0:
+            torch.save(net.state_dict(), f'./saved_models_{args.run_name}/HEAR_latest.pth')
+
+
+def train(args, device):
+    # training params
+    batch_size = args.batch_size
+    lr = args.lr
+    max_epoch = args.max_epoch
+    optim_level = args.optim_level
+
+    # initializing main models
+    G = AEI_Net(args.backbone, num_blocks=args.num_blocks, c_id=512).to(device)
+    G.eval()
+
+    net = HearNet()
+    net.train()
+    net.to(device)
+
+    netArc = convert(args.arcface_onnx_path)
+    netArc = netArc.cuda()
+    netArc.eval()
+
+    opt = optim.Adam(net.parameters(), lr=lr, betas=(0, 0.999))
+    net, opt = amp.initialize(net, opt, opt_level=optim_level)
+        
+    if args.pretrained:
+        try:
+            if args.G_path:
+                G.load_state_dict(torch.load(args.G_path, map_location=torch.device('cpu')), strict=False)
+                print("Loaded pretrained weights for G")
+            if args.HEAR_path:
+                net.load_state_dict(torch.load(args.HEAR_path, map_location=torch.device('cpu')), strict=False)
+                print("Loaded pretrained weights for HEARNET")
+        except FileNotFoundError as e:
+            print("Not found pretrained weights. Continue without any pretrained weights.")
+    
+    dataset = AugmentedOcclusions(args.faces_data,
+                                  args.hands_data,
+                                  args.shapes_data, same_prob=0.5)
+        
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=8, drop_last=True)
+
+    print(torch.backends.cudnn.benchmark)
+    
+    for epoch in range(0, max_epoch):
+        train_one_epoch(G,
+                        net,
+                        opt,
+                        netArc,
+                        args,
+                        dataloader,
+                        device,
+                        epoch)
+
+def main(args):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if not torch.cuda.is_available():
+        print('cuda is not available. using cpu. check if it\'s ok')
+    
+    print("Starting traing")
+    train(args, device=device)
+
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # dataset params
+    parser.add_argument('--faces_data', default='./examples/heartest/faces', help='Path to the faces dataset.')
+    parser.add_argument('--hands_data', default='./examples/heartest/hands', help='Path to the hands dataset.')
+    parser.add_argument('--shapes_data', default='./examples/heartest/shapes', help='Path to the shapes dataset.')
+    parser.add_argument('--arcface_onnx_path', default=None, help='Path to source arcface emb extractor')
+    parser.add_argument('--G_path', default='', help='Path to pretrained weights for G. Only used if pretrained=True')
+    parser.add_argument('--HEAR_path', default='', help='Path to pretrained weights for HEARNET. Only used if pretrained=True')
+    # training params you may want to change
+    parser.add_argument('--backbone', default='unet', const='unet', nargs='?', choices=['unet', 'linknet', 'resnet'], help='Backbone for attribute encoder')
+    parser.add_argument('--num_blocks', default=2, type=int, help='Numbers of AddBlocks at AddResblock')
+    parser.add_argument('--pretrained', default=True, type=bool, help='If using the pretrained weights for training or not')
+    # info about this run
+    parser.add_argument('--use_wandb', default=False, type=bool, help='Use wandb to track your experiments or not')
+    parser.add_argument('--run_name', required=True, type=str, help='Name of this run. Used to create folders where to save the weights.')
+    parser.add_argument('--wandb_project', default='your-project-name', type=str)
+    parser.add_argument('--wandb_entity', default='your-login', type=str)
+    # training params you probably don't want to change
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser.add_argument('--lr', default=4e-4, type=float)
+    parser.add_argument('--max_epoch', default=2000, type=int)
+    parser.add_argument('--max_steps', default=-1, type=int)
+    parser.add_argument('--show_step', default=10, type=int)
+    parser.add_argument('--save_epoch', default=1000, type=int)
+    parser.add_argument('--optim_level', default='O2', type=str)
+    args = parser.parse_args()
+    
+    if args.use_wandb==True:
+        wandb.init(project=args.wandb_project, entity=args.wandb_entity, settings=wandb.Settings(start_method='fork'))
+        config = wandb.config
+        config.dataset_path = args.dataset_path
+        config.pretrained = args.pretrained
+        config.run_name = args.run_name
+        config.G_path = args.G_path
+        config.batch_size = args.batch_size
+        config.lr = args.lr
+        config.max_epoch = args.max_epoch
+    elif not os.path.exists('./images'):
+        os.mkdir('./images')
+    
+    if not os.path.exists(f'./saved_models_{args.run_name}'):
+        os.mkdir(f'./saved_models_{args.run_name}')
+    
+    main(args)
--- utils/training/Dataset.py
+++ utils/training/Dataset.py
@@ -8,6 +8,7 @@ import os
 import cv2
 import tqdm
 import sys
+import numpy as np
 sys.path.append('..')
 # from utils.cap_aug import CAP_AUG
     
@@ -129,3 +130,80 @@ class FaceEmbedVGG2(TensorDataset):
 
     def __len__(self):
         return self.N
+
+def compose_occlusion(face_img, occlusions):
+    h, w, c = face_img.shape
+    if len(occlusions) == 0:
+        return face_img
+    for occlusion in occlusions:
+        # scale
+        scale = random.random() * 0.5 + 0.5
+        # occlusion = cv2.resize(occlusion, (), fx=scale, fy=scale)
+        # rotate
+        R = cv2.getRotationMatrix2D((occlusion.shape[0]/2, occlusion.shape[1]/2), random.random()*180-90, scale)
+        occlusion = cv2.warpAffine(occlusion, R, (occlusion.shape[1], occlusion.shape[0]))
+        print(f"occlusion.shape {occlusion.shape}")
+        oh, ow, _ = occlusion.shape
+        oc_color = occlusion[:, :, :3]
+        oc_alpha = occlusion[:, :, 3].astype(np.float) / 255.
+        oc_alpha = np.expand_dims(oc_alpha, axis=2)
+        tmp = np.zeros([h+oh, w+ow, c])
+        tmp[oh//2:oh//2+h, ow//2:ow//2+w, :] = face_img
+        cx = random.randint(int(ow / 2) + 1, int(w + ow / 2) - 1)
+        cy = random.randint(int(oh / 2) + 1, int(h + oh / 2) - 1)
+        stx = cx - int(ow / 2)
+        sty = cy - int(oh / 2)
+        tmp[sty:sty+oh, stx:stx+ow, :] = oc_color * oc_alpha + tmp[sty:sty+oh, stx:stx+ow, :] * (1-oc_alpha)
+        face_img = tmp[oh//2:oh//2+h, ow//2:ow//2+w, :].astype(np.uint8)
+    return face_img
+
+class AugmentedOcclusions(TensorDataset):
+    def __init__(self, face_sets, hand_sets, obj_sets, same_prob=0.5):
+        self.same_prob = same_prob
+        self.face_img_paths = glob.glob(f'{face_sets}/*/*.*g')
+        self.hands_data = glob.glob(f'{hand_sets}/*.png')
+        self.obj_data = glob.glob(f'{obj_sets}/*/*.png')
+        self.transforms_base = transforms.Compose([
+            transforms.ColorJitter(0.2, 0.2, 0.2, 0.01),
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    def gen_occlusion(self):
+        p = random.random()
+        occlusions = []
+        if p < 0.25: # no occlusion
+            pass
+        elif p < 0.5: # only hand
+            hand_img = cv2.imread(self.hands_data[random.randint(0, len(self.hands_data)-1)], cv2.IMREAD_UNCHANGED)
+            occlusions.append(hand_img)
+        elif p < 0.75: # only object
+            obj_img = cv2.imread(self.obj_data[random.randint(0, len(self.obj_data)-1)], cv2.IMREAD_UNCHANGED)
+            occlusions.append(obj_img)
+        else: # both
+            hand_img = cv2.imread(self.hands_data[random.randint(0, len(self.hands_data)-1)], cv2.IMREAD_UNCHANGED)
+            occlusions.append(hand_img)
+            obj_img = cv2.imread(self.obj_data[random.randint(0, len(self.obj_data)-1)], cv2.IMREAD_UNCHANGED)
+            occlusions.append(obj_img)
+        return occlusions
+
+    def __getitem__(self, item):
+        face_path = self.face_img_paths[item]
+        face_img = cv2.imread(face_path)[:, :, ::-1]
+
+        Xs = face_img
+        p = random.random()
+        if p > self.same_prob:
+            Xt_path = self.face_img_paths[random.randint(0, len(self.face_img_paths)-1)]
+            Xt = cv2.imread(Xt_path)[:, :, ::-1]
+            Xt = compose_occlusion(Xt, self.gen_occlusion())
+            same_person = 0
+        else:
+            Xt = compose_occlusion(face_img, self.gen_occlusion())
+            same_person = 1
+        return self.transforms_base(Image.fromarray(Xs)), self.transforms_base(Image.fromarray(Xt)), same_person
+
+    def __len__(self):
+        return len(self.face_img_paths)
